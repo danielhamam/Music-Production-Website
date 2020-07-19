ce9ef452a19c08a20d54560e26ee4eb1989629df
Daniel Hamam <danielhamam@Daniels-MacBook-Air.local>
2020-07-19 15:54:31 -0400

    Took out Drum Kits button (not needed yet)

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -38,7 +38,7 @@ class Navbar extends Component {
 				<div id="navbar_buttons_container"> 
                 	<button type="button" onClick={this.home_scrollFunction} id="home_button" className="navbar_button"> HOME </button>
                 	<button type="button" onClick={this.catalog_scrollFunction} id="catalog_button" className="navbar_button"> BEAT CATALOG </button>
-					<button type="button" id="kits_button" className="navbar_button" > DRUM KITS </button>
+					{/* <button type="button" id="kits_button" className="navbar_button" > DRUM KITS </button> */}
                 	<button type="button" onClick={this.pricing_scrollFunction} id= "pricing_button" className="navbar_button"> LICENSE INFO </button>
                 	<button type="button" onClick={this.contact_scrollFunction} id="contact_button" className="navbar_button"> CONTACT </button>
 				</div>
