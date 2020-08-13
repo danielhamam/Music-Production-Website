bcd9c51fe02d13b93049d4ae9b23175ddb062cbf
Daniel Hamam <danielhamam@Daniels-MacBook-Air.local>
2020-08-12 23:49:30 -0400

    Latest Version (CSS GRID)

--- src/components/home_screen/contact/ContactInfo.js
+++ src/components/home_screen/contact/ContactInfo.js
@@ -6,21 +6,21 @@ render() {
   return (
         <div id="contact_info_container">
             <div id="contact_information"> 
-                <form> 
+            <form> 
 
-                <label> Your Name: </label>
-                <input type="text" name="fname" />
-                <label> Your Email: </label>
-                <input type="text" />
-                <label> Subject: </label>
-                <input type="text" />
-                <label> Your Message: </label>
-                <input type="text" />
-                <br />
-                <br />
-                <input id="contact_submit_button" type="submit" value="SEND" />
+            <label> Your Name: </label>
+            <input id="contact_name" type="text" name="fname" />
+            <label> Your Email: </label>
+            <input id="contact_email" type="text" />
+            <label> Subject: </label>
+            <input id="contact_subject" type="text" />
+            <label> Your Message: </label>
+            <textarea id="contact_message" cols="10" rows="10" type="text" />
+            <br />
+            <br />
+            <input id="contact_submit_button" type="submit" value="SEND" />
 
-                </form>
+            </form>
 
 
             </div>
